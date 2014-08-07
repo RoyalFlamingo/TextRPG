651bf6504e4ee8e51323c3b05f9d29ac364cf839
RoyalFlamingo <alfred.hromas@gmail.com>
2014-08-07 10:24:03 +0200

    First Build

---
+++ Locations.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace DetGameTho
+{
+    class Locations
+    {
+        public int ID;
+        public string Name;
+        public string Discription;
+        private int Status; //99 = Default
+        private Random locrandom = new Random();
+        private string[,] LocNames = new string[100, 2];
+        
+
+        //Constructor
+
+        public Locations()
+        {
+            ID = 0;
+            Name = "Null";
+            Discription = "Null";
+            Status = 99;
+        }
+
+        public Locations(int id)
+        {
+            ID = id;
+            int i = 0;
+            // Randomberechnung
+            string filePath = System.IO.Path.GetFullPath("Locs.txt");
+            using (StreamReader sr = new StreamReader(filePath))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    i++;
+                }
+                using (StreamReader sr2 = new StreamReader(filePath))
+                    for (int u = 0; u <= i; u++)
+                    {
+                        LocNames[u, 0] = sr2.ReadLine();
+                    }
+                filePath = System.IO.Path.GetFullPath("LocsDiscription.txt");
+                using (StreamReader sr3 = new StreamReader(filePath))
+                {
+                    for (int u = 0; u <= i; u++)
+                    {
+                        LocNames[u, 1] = sr3.ReadLine();
+                    }
+                }
+
+            }
+            int rd = locrandom.Next(0, i);
+            Name = LocNames[rd,0];
+            Discription = LocNames[rd, 1];
+            
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.IO;
+
+namespace DetGameTho
+{
+    class Program
+    {
+        static int[,] i_map = new int[100, 100];
+        static int i_curpos_x = new int();
+        static int i_curpos_y = new int();
+        static int i_lastid = new int();
+        static Locations[] loc1 = new Locations[999];
+        static Random random = new Random();
+
+        static void Main(string[] args)
+        {
+            //INIT
+            i_lastid = 0;
+            i_curpos_x = 50;
+            i_curpos_y = 50;
+            Console.WriteLine("Willkommen zum TestRPG!");
+            Abfrage: // GOTO ABFRAGE
+            Console.WriteLine("Gebe V ein um nach Vorne zu gehen, gebe Z ein um Zurück zu gehen, L um nach Links zu gehen und R um nach Rechts zu gehen.");
+            ConsoleKeyInfo info = Console.ReadKey();
+            Console.Clear(); //Bisheriger Text wird gecleart
+            //Richtungsabfrage und Bewegung
+            if (info.KeyChar == 'V')
+            #region VORWÄRTS
+            {
+                Thread.Sleep(500);
+                i_curpos_y++;
+                if (i_map[i_curpos_x, i_curpos_y] == 0)  //Wenn Ort noch nicht besucht, dann neue Location mit neuer ID anlegen
+                {
+                    int nextid = i_lastid++;
+                    loc1[nextid] = new Locations(nextid);
+                    i_map[i_curpos_x, i_curpos_y] = loc1[nextid].ID;
+                }
+                //Console.WriteLine(Convert.ToString(i_map[i_curpos_x, i_curpos_y]));
+                //Console.WriteLine(Convert.ToString(i_curpos_x + " " + i_curpos_y));
+                Console.WriteLine(loc1[(i_map[i_curpos_x,i_curpos_y])].Name);
+                Console.WriteLine(loc1[(i_map[i_curpos_x, i_curpos_y])].Discription);
+                Console.WriteLine(" ");
+                goto Abfrage;
+            }
+            #endregion
+            if (info.KeyChar == 'Z')
+            #region ZURÜCK
+            {
+                Thread.Sleep(500);
+                i_curpos_y--;
+                if (i_map[i_curpos_x, i_curpos_y] == 0)
+                {
+                    int nextid = i_lastid++;
+                    loc1[nextid] = new Locations(nextid);
+                    i_map[i_curpos_x, i_curpos_y] = loc1[nextid].ID;
+                }
+                //Console.WriteLine(Convert.ToString(i_map[i_curpos_x, i_curpos_y]));
+                //Console.WriteLine(Convert.ToString(i_curpos_x+" "+i_curpos_y));
+                Console.WriteLine(loc1[(i_map[i_curpos_x, i_curpos_y])].Name);
+                Console.WriteLine(loc1[(i_map[i_curpos_x, i_curpos_y])].Discription);
+                Console.WriteLine(" ");
+                goto Abfrage;
+            }
+            #endregion
+            if (info.KeyChar == 'L')
+            #region LINKS
+            {
+                Thread.Sleep(500);
+                i_curpos_x--;
+                if (i_map[i_curpos_x, i_curpos_y] == 0)
+                {
+                    int nextid = i_lastid++;
+                    loc1[nextid] = new Locations(nextid);
+                    i_map[i_curpos_x, i_curpos_y] = loc1[nextid].ID;
+                }
+                //Console.WriteLine(Convert.ToString(i_map[i_curpos_x, i_curpos_y]));
+                //Console.WriteLine(Convert.ToString(i_curpos_x + " " + i_curpos_y));
+                Console.WriteLine(loc1[(i_map[i_curpos_x, i_curpos_y])].Name);
+                Console.WriteLine(loc1[(i_map[i_curpos_x, i_curpos_y])].Discription);
+                Console.WriteLine(" ");
+                goto Abfrage;
+            }
+            #endregion
+            if (info.KeyChar == 'R')
+            #region RECHTS
+            {
+                Thread.Sleep(500);
+                i_curpos_x++;
+                if (i_map[i_curpos_x, i_curpos_y] == 0)
+                {
+                    int nextid = i_lastid++;
+                    loc1[nextid] = new Locations(nextid);
+                    i_map[i_curpos_x, i_curpos_y] = loc1[nextid].ID;
+                }
+                //Console.WriteLine(Convert.ToString(i_map[i_curpos_x, i_curpos_y]));
+                //Console.WriteLine(Convert.ToString(i_curpos_x + " " + i_curpos_y));
+                Console.WriteLine(loc1[(i_map[i_curpos_x, i_curpos_y])].Name);
+                Console.WriteLine(loc1[(i_map[i_curpos_x, i_curpos_y])].Discription);
+                Console.WriteLine(" ");
+                goto Abfrage;
+            }
+            #endregion
+
+            Console.WriteLine("Ungültiger Befehl!");
+            goto Abfrage;
+        }
+
+        public void LocationsEinlesen()
+        {
+            
+        }
+    }
+}
